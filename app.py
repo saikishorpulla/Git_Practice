1019861733729d0d7df804c5c52697b8d5c4a431
Sai Kishor <pullasaikishor@gmail.com>
2026-02-03 14:25:23 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,8 @@
+#python program for even num & odd num
+import json
+for i in range(100): 
+    if i%2 == 0:
+        print(f'{i} is an even number')
+    else:
+        print(f'{i} is an even number')
+    
